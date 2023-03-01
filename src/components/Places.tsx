4af987d0a2d109e52da95f1962240ada6f7e6888
0xukhezo <ukhezo.web3@gmail.com>
2023-03-01 15:37:32 +0100

    fix:Remove Bg and gradient overflow

--- src/components/Places.tsx
+++ src/components/Places.tsx
@@ -73,7 +73,7 @@ export function Places() {
           </div>
         </div>
       ) : (
-        <div className="flex h-screen flex-col items-center bg-gradient-to-b from-gray-100 to-blue-500 ">
+        <div className="flex h-screen flex-col items-center bg-gray-100">
           <div className="overflow-auto w-screen h-full text-center">
             <div className="flex flex-col items-center">
               <h1 className="font-pathway text-4xl text-studentsBlue-200 mt-10">
@@ -88,7 +88,7 @@ export function Places() {
                 baloncesto, el Estu.
               </div>
             </div>
-            <div className="text-center pb-16 bg-hero">
+            <div className="text-center pb-16">
               <div className="grid justify-items-center ml-16 ">
                 {places?.map((poap: any, index: number) => (
                   <div className="-mt-4 ">
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <div className="flex items-center justify-center  p-4 z-50">
+    <div className="flex items-center justify-center  p-4 z-50 bg-gradient-to-b from-gray-100 to-blue-500">
       <img src={"/footr_mitico.svg"} className="h-10"></img>
     </div>
   );
